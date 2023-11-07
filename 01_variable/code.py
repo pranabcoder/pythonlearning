443e5ac8d6805ae35202218ba405fbff25953675
pranabcoder <pranabit6@gmail.com>
2023-11-07 20:43:08 +0530

    first commit 01_variable

---
+++ 01_variable/code.py
@@ -0,0 +1,23 @@
+# How to define a variable
+
+x = 15
+price = 9.99
+discount = 0.2
+result = price * (1 - discount)
+
+# print the value to user
+print(result)
+
+name = "Rolf"
+name = "Bob"
+print(name * 2)
+
+# Changing variable value
+a = 25
+b = a
+print(a)
+print(b)
+b = 17
+print(a)
+print(b)
+
